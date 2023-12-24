5b966c7e94181b54b3f90038d7af39b7fd14f182
Ekra Chowdhury Risha <135812254+Ekrachowdhuryrisha@users.noreply.github.com>
2023-12-24 19:34:28 +0600

    Add files via upload

---
+++ 61a-UltraFastMathematician.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   string a,b;
+   cin>>a>>b;
+
+    for(int i=0; i<a.size(); i++)
+    {
+      //cout<<a[i]<<" "<<b[i]<<" ";
+        if(a[i]!=b[i])
+        {
+            cout<<"1";
+        }
+        else
+        {
+            cout<<"0";
+        }
+    }
+
+    return 0;
+}
