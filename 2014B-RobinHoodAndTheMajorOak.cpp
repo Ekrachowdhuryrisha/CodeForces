0ce704fb98aedcbcbc12600f1b1a0c4a8f72c300
Ekra Chowdhury Risha <135812254+Ekrachowdhuryrisha@users.noreply.github.com>
2024-09-25 10:24:26 +0600

    Update 2014B-RobinHoodAndTheMajorOak.cpp

--- 2014B-RobinHoodAndTheMajorOak.cpp
+++ 2014B-RobinHoodAndTheMajorOak.cpp
@@ -1,34 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-int t;
-cin>>t;
-while(t-->0){
-    int n,k;
-    cin>>n>>k;
-    int sum=0, temp= n-k;
-    if(n<=k){
-    for( int i=1; i<=n; i++){
-        sum= sum+ pow(i,i);
-    }
-    }
-    else{
-        for(int i=n; i>=(n-temp); i--){
-            sum= sum+pow(i,i);
+int main()
+{
+    int t;
+    cin>>t;
+    while(t-->0)
+    {
+        int n,k;
+        cin>>n>>k;
+
+        int  sum=((n+1)*n/2 - (n-k+1)/2);
+
+        if(sum%2==0)
+        {
+            cout<<"yes"<<endl;
         }
+        else
+        {
+            cout<<"no"<<endl;
+        }
+
     }
 
-
-    if(sum%2==0){
-        cout<<"yes"<<endl;
-    }
-    else{
-        cout<<"no"<<endl;
-    }
-
-}
-
-
-
-return 0;
+    return 0;
 }
